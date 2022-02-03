58f776c24e4e792048d5d60371702d09c3c844d5
troychibbaro <troybrchibbaro@gmail.com>
2022-02-03 16:22:37 -0500

    Updated README.md, added apple_accessory_searcher.py.

--- README.md
+++ README.md
@@ -2,13 +2,18 @@
 This repository contains some sample Python scripts used to 
 extract data from websites. The following are brief descriptions of 
 each project, and I will try to add new ones periodically.
+
 ---
 ### Wikipedia Citation Scraper
-Input: URL of desired Wikipedia article
+    Input: URL of desired Wikipedia article
 
-Output: A file named "(article name) Citations.txt" that contains
-that pages citations. If no citations were found, the file does not 
-get created. Any errors are printed to console. 
+    Output: A file named "(article name) Citations.txt" that contains
+    that pages citations. If no citations were found, the file does not 
+    get created. Any errors are printed to console. 
 
 ---
+### Apple Online Store Product Search
+    Input: Product name that you would like to search.
 
+    Output: Prints all search results to console as follows:
+    "Product Name" - "Product Price"
---
+++ apple_accessory_searcher.py
@@ -0,0 +1,45 @@
+import requests
+from requests import exceptions
+from bs4 import BeautifulSoup
+from urllib.error import HTTPError
+
+
+def get_search_results(product: str):
+    try:
+        # Store desired product, format URL
+        URL = "https://www.apple.com/us/search/{0}?src=alp"
+        product = product.replace(" ", "-")
+        URL = URL.format(product)
+
+        # Request page and instantiate bs4 object
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+
+        # search for all product results
+        product_results = soup.find_all('div', class_="rf-producttile-info")
+        # print(product_results)
+
+        # store product data, return dictionary if results are present
+        all_products = []
+        if len(product_results) > 0:
+            for p in product_results:
+                product_name = p.find('h2', class_="rf-producttile-name").text.strip()
+                product_price = p.find('span', class_="rf-producttile-pricecurrent").text.strip()
+                all_products.append({
+                    "name": product_name,
+                    "price": product_price,
+                })
+            return all_products
+    except (HTTPError, requests.exceptions.RequestException, ValueError):
+        return None
+
+
+if __name__ == "__main__":
+    product = input("Enter desired product name: ")
+    results = get_search_results(product)
+    if results:
+        print(f'Found {len(results)} results for "{product}"')
+        for res in results:
+            print(f'{res["name"]}: {res["price"]}')
+    else:
+        print(f'No product "{product}" found')
